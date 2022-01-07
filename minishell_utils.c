b857ae0bacfd07e0d6072cfad626604083666c7c
Mohammed amine Ajdahim <majdahim@e2r10p5.1337.ma>
2022-01-07 19:09:06 +0100

    n

--- minishell_utils.c
+++ minishell_utils.c
@@ -3,21 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yer-raki <yer-raki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: majdahim <majdahim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/02 13:28:12 by yer-raki          #+#    #+#             */
-/*   Updated: 2021/06/04 11:48:47 by yer-raki         ###   ########.fr       */
+/*   Created: 2022/01/07 15:16:37 by majdahim          #+#    #+#             */
+/*   Updated: 2022/01/07 15:34:08 by majdahim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-	int i;
+	int	i;
 
 	i = 0;
-
 	if (!s1)
 		return (s2[i]);
 	else if (!s2)
@@ -29,14 +28,19 @@ int		ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
-int		ft_strlen2(char **w)
+char	**ft_realloc_2(char **old, size_t old_size, size_t new_size)
 {
-	int i;
+	char	**new;
+	int		i;
 
+	new = malloc(sizeof(char *) * (new_size + 1));
 	i = 0;
-	while (w[i])
+	while (i < (int)old_size)
+	{
+		new[i] = old[i];
 		i++;
-	return (i);
+	}
+	return (new);
 }
 
 void	ft_putchar(int fd, char c)
@@ -46,7 +50,7 @@ void	ft_putchar(int fd, char c)
 
 void	ft_putstr(int fd, char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (s[i])
@@ -56,87 +60,12 @@ void	ft_putstr(int fd, char *s)
 	}
 }
 
-void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+int	ft_strlen2(char **w)
 {
-	void	*new;
-	
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new = malloc(new_size * sizeof(char));
-	if (!new)
-		return (NULL);
-	if (ptr != NULL || !old_size)
-	{
-		if (old_size >= new_size)
-			ft_memcpy(new, ptr, new_size);
-		else
-			ft_memcpy(new, ptr, old_size);
-		free(ptr);
-	}
-	return (new);
-}
-
-// char	**ft_realloc_2(char **old, size_t old_size, size_t new_size)
-// {
-// 	char **new;
-// 	int		i;
-	
-// 	new = malloc(sizeof(char *) * (new_size + 1));
-// 	i = 0;
-// 	while (i < (int)old_size)
-// 	{
-// 		new[i] = old[i];
-// 		i++;
-// 	}
-// 	return new;
-// }
-void	ft_putnbr(int fd, int n)
-{
-	unsigned int nb;
-
-	if (n < 0)
-	{
-		ft_putchar(fd, '-');
-		n = -n;
-	}
-	nb = n;
-	if (nb > 9)
-	{
-		ft_putnbr(fd, nb / 10);
-		ft_putchar(fd, (nb % 10) + '0');
-	}
-	else
-		ft_putchar(fd, nb + '0');
-}
-
-void	free_t2(char **w)
-{
-	int i;
-	int	x;
+	int	i;
 
 	i = 0;
-	x = ft_strlen2(w);
-	while (x > i)
-	{
-		free(w[i]);
+	while (w[i])
 		i++;
-	}
-	free(w);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (i);
 }
